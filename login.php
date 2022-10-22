3be07a38335043c75988cd601b430393461dcb4d
Ranatza <69979310+Ranatza@users.noreply.github.com>
2022-10-22 17:52:11 -0500

    Update login.php

---
+++ login.php
@@ -0,0 +1,20 @@
+<html>
+  <body>
+    $db = mysqli_connect("localhost", "INFX371", "P*ssword", "wiki");
+    <form action="action_page.php" method="post">
+      <div class="container">
+        <label for="username"><b>Username</b></label>
+        <input type="text" placeholder="Enter Username" name="username" required>
+
+        <label for="password"><b>Password</b></label>
+        <input type="password" placeholder="Enter Password" name="password" required>
+
+        <button type="submit">Login</button>
+      </div>
+
+      <div class="container">
+        <button type="button">Create Profile</button>
+      </div>
+    </form>
+  </body>
+</html>
