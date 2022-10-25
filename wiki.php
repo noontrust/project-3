a8be4d27ddf20901767143826c43d297dab104bc
LColeman137 <116465104+LColeman137@users.noreply.github.com>
2022-10-24 19:38:55 -0500

    Added header and footer, updated files
    Updated wiki and addArticle with the header and footer. Updated wiki.php to have a sidebar and updated the connection code to run because it was broken. Updated style.css to account for the sidebar. Separated the header and footer from index.php so they could be used on all pages.

--- wiki.php
+++ wiki.php
@@ -6,6 +6,7 @@
   if(!isset($_SESSION["sessionId"])){header("Location: createUser.php";
   include "database.php";}
 */
+    include "header.php";
     session_start();
     //query the articles
     $HOST = "localhost";
@@ -20,35 +21,42 @@
 <head>
   <title>Cool Facts Wiki</title>
   <meta charset="utf-8">
+  <link href="style.css" type="text/css" rel="stylesheet">
 </head>
 <body>
-  <div class="sidebar">
+  <div class="sideBar">
     <?php
     //possible search bar
     //place links to each wiki page
      foreach ($wikiResult as $row){
-      echo "<a href=wiki.php/?shortTitle='$row[1]' title='$row[2]'>$row[1]</a>";
+      echo "<a href=wiki.php?shortTitle=$row[1] title='$row[2]'>$row[2]</a>";
     }
     ?>
-      <a href="wiki.php?wikiPage=cats">Cats</a><?php=//"$wikiPage"?>
   </div>
-  <div>
-    <h1>Home</h1>
-    <h4>Use the navigation bar to view wikis, or add you own below!</h4>
-    <hr>
+  <div class="main">
     <?php
       if(isset($_GET["shortTitle"])){
-        $row = $_GET["shortTitle"];
-        $articleQuery = "SELECT * FROM article WHERE short_title = '$row'";
-        $article = $conn->query($articleQuery)->fetch();
+        $shortTitle = $_GET["shortTitle"];
+        unset($_GET["shortTitle"]);
+        $articleQuery = "SELECT * FROM article WHERE article_short_title = '$shortTitle';";
+        $stmt = $conn->query($articleQuery);
+        $article = $stmt->fetch_row();
+        echo "<h1>" . $article[2] ."</h1>";
+        echo "<h4>This article is all about " . $article[2] ."!</h4>";
         $title = $article[1];
         $info = $article[2];
         echo $article[3];
+        //add link to addArticle here
+      }
+      else{
+        echo "<h1>Home</h1>";
+        echo "<h4>Use the navigation bar to view wikis, or add you own below!</h4>";
+        echo "<form action='addArticle.php' method='post'>
+          <input type='submit' value='Upload Article' name='articleSubmit'>
+        </form>";
       }
     ?>
-    <form action="addArticle.php" method="post"
-      <input type="submit" value="Upload Article" name="articleSubmit">
-    </form>
   </div>
+  <?php include "footer.php";?>
 </body>
 </html>
