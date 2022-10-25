8ccbf10c4c6917dd53d713423efc296318ad7566
noontrust <82079179+noontrust@users.noreply.github.com>
2022-10-25 13:05:38 -0500

    commit

---
+++ addArticle.php
@@ -0,0 +1,14 @@
+<?php
+ ?>
+<html lang="en">
+<head>
+  <title>Cool Facts Wiki</title>
+  <meta charset="utf-8">
+  <link href="style.css" type="text/css" rel="stylesheet">
+</head>
+<form action="upload.php" method="post" enctype="multipart/form-data">
+Select image to upload:
+<input type="file" name="imageUpload" id="imageUpload">
+<input type="submit" value="Upload Image" name="submit">
+</form>
+</html>
