ce5cfaa598fbf3db9d87a74d280c11e3f4ced9e6
LColeman137 <116465104+LColeman137@users.noreply.github.com>
2022-10-30 15:01:45 -0500

    uploads articles to the database
    Also adds images to the images folder /projectFolder/images. Redirects to the new article after creation.

--- upload.php
+++ upload.php
@@ -1,12 +1,37 @@
 <?php
 /*
-  The following code is from https://www.w3schools.com/php/php_file_upload.asp
+  Must add other updates to database from here.
 */
-$target_dir = "images/";
-$target_file = $target_dir . basename($_FILES["imageUpload"]["name"]);
-
-// Check if image file is a actual image or fake image
+include_once "database.php";
 if(isset($_POST["submit"])) {
-  $check = getimagesize($_FILES["imageUpload"]["tmp_name"]);
-  move_uploaded_file($_FILES["imgUpload"]["tmp_name"], $target_file);
+  unset($_POST["submit"]);
+  $query = "";
+  //insert data into database
+  $title = trim($_POST["title"]);
+  $short_title = str_replace(' ', '', $title);
+  $body = trim($_POST["body"]);
+  $query .= "INSERT INTO `article` VALUES " . "(NULL, '$short_title', '$title', '$body'";
+
+  //This adds the photo upload to the images folder
+  $target_dir = "images/";
+  if(isset($_FILES["imageUpload"]) && file_exists($_FILES["imageUpload"]["tmp_name"])){
+    $target_file = $target_dir . basename($_FILES["imageUpload"]["name"]);
+    move_uploaded_file($_FILES["imageUpload"]["tmp_name"], $target_file);
+    unset($_FILES["imageUpload"]);
+    $query .=", '$target_file'";
+  }
+  else {
+    $query .= ", NULL";
+  }
+  if(isset($_POST["birthdate"])){
+    $birthdate = $_POST["birthdate"];
+    $query .= ", '$birthdate'";
+  }
+  else {
+    $query .=", NULL";
+  }
+  $conn->query($query .");");
+  echo $query.");";
+  header("Location: wiki.php?shortTitle=".$short_title);
+}
 ?>
