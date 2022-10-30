ce8f7eefc3c2cc02b30f86a1cfb8cb5d15925a48
LColeman137 <116465104+LColeman137@users.noreply.github.com>
2022-10-30 14:11:38 -0500

    added queries for wiki.php

--- database.php
+++ database.php
@@ -1,13 +1,9 @@
-<?php 
-$db_host = 'localhost';
-$db_name = 'wiki';
-$db_user = 'root';
-$db_password = '';
-
-$mysqli = new mysqli($db_host, $db_user, $db_password, $db_name);
-
-if($mysqli->connect_errno){
-    printf("Connection failed: %s\n", $mysqli->connect_error);
-    exit();
-}
+<?php
+$HOST = "localhost";
+$USERNAME = "root";
+$PASSWORD = "";
+$DB_NAME = "wiki";
+$conn = new mysqli($HOST, $USERNAME, $PASSWORD, $DB_NAME);
+$stmt = $conn->query("SELECT * FROM article");
+$wikiResult = $stmt->fetch_all();
 ?>
