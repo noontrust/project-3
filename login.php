1317b5c4ed30fef5a2b1ea755fee4595dd50aaa3
Ranatza <69979310+Ranatza@users.noreply.github.com>
2022-11-01 08:34:31 -0500

    working create user and login pages
    with hash and salt!

--- login.php
+++ login.php
@@ -1,6 +1,26 @@
 <?php
   $db = mysqli_connect("localhost", "INFX371", "P*ssword", "wiki");
+  if (!$db) {
+    echo "Connection failed!";
+  } 
+
+  if (isset($_POST['username']) && isset($_POST['password'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $hash = mysqli_fetch_assoc(mysqli_query($db, "SELECT password FROM `usr` WHERE username = '$username'"));
+
+    if (password_verify($password, $hash['password'])) {
+      session_start();
+      header("Location: index.php");
+      exit();
+    }
+    else {
+      echo "Incorrect Username or Password";
+    }
+  }
 ?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -29,7 +49,7 @@
           <input type="password" placeholder="Enter Password" name="password" required>
           <p></p>
           <div id="logButtons">
-          <a id="registerButton" href="">Register new account</a>
+          <a id="registerButton" href="./createuser.php">Register new account</a>
           <button id="loginButton" type="submit">Login</button>
           </div>
       </form>
