6c110b112097160bf670a296d04610c10fa3cd28
Ranatza <69979310+Ranatza@users.noreply.github.com>
2022-11-01 19:20:52 -0500

    update

--- createuser.php
+++ createuser.php
@@ -26,59 +26,40 @@
 ?>
 
 <!DOCTYPE html>
-
 <html>
-  <head>
-    <meta charset="utf-8" />
-    <title>Wiki</title>
+  <?php include "header.php";?>
 
-    <link rel="stylesheet" href="style.css" type="text/css"/>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Sarabun:ital@0;1&display=swap" rel="stylesheet">
-  </head>
+  <a href="./login.php">< Back</a>
 
-  <body>
-    <header>
-      <div class="topBar">
-        <div class="logo">
-          <p id="wikiTitle">Wiki of Things</p>
-        </div>
-        <div id="filler"></div>
-      </div>
-    </header>
-
-    <a href="./login.php">< Back</a>
-
-    <div class="loginBox">   
-      <form action="createuser.php" method="post">
-        <label for="username"><b>Username</b></label>
-        <input type="text" placeholder="Enter Username" name="username" required>
-        <p></p>
+  <div class="loginBox">   
+    <form action="createuser.php" method="post">
+      <label for="username"><b>Username</b></label>
+      <input type="text" placeholder="Enter Username" name="username" required>
+      <p></p>
           
-        <label for="password"><b>Password</b></label>
-        <input type="password" placeholder="Enter Password" name="password" required>
-        <p></p>
+      <label for="password"><b>Password</b></label>
+      <input type="password" placeholder="Enter Password" name="password" required>
+      <p></p>
         
-        <label for="password-retype"><b>Re-Type Password</b></label>
-        <input type="password" placeholder="Re-enter Password" name="password-retype" required>
-        <p></p>
+      <label for="password-retype"><b>Re-Type Password</b></label>
+      <input type="password" placeholder="Re-enter Password" name="password-retype" required>
+      <p></p>
 
-        <div id="logButtons">
-          <button id="createuser" type="submit">Create Account</button>
-        </div>
-        <p></p>
+      <div id="logButtons">
+        <button id="createuser" type="submit">Create Account</button>
+      </div>
+      <p></p>
 
-        <div class="error">
-          <?php 
-            if (isset($error)) {
-              echo $error;
-            }
-          ?>
-        </div>
-      </form>
-    </div>
+      <div class="error">
+        <?php 
+          if (isset($error)) {
+            echo $error;
+          }
+        ?>
+      </div>
+    </form>
+  </div>
 
-    <footer>Copyright 2022 &copy</footer>
+  <?php include "footer.php";?>
   </body>
 </html>
