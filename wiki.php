78aeaca95553bc8dda232aaac651723d47cef0ef
noontrust <82079179+noontrust@users.noreply.github.com>
2022-11-02 18:16:09 -0500

    fixed wiki layout

--- wiki.php
+++ wiki.php
@@ -9,6 +9,8 @@
   ?>
 <html lang="en">
 <?php include "header.php" ?>
+<div class="bigcontainer">
+  <div class="container">
   <div class="sideBar">
     <?php
     //place links to newest 5 wiki page
@@ -17,29 +19,31 @@
     }
     ?>
   </div>
-  <div class="main">
+  <div class="articleDivs">
     <?php
-      if(isset($_GET["shortTitle"])){
         $shortTitle = $_GET["shortTitle"];
         unset($_GET["shortTitle"]);
         $articleQuery = "SELECT * FROM article WHERE article_short_title = '$shortTitle';";
         $stmt = $conn->query($articleQuery);
         $article = $stmt->fetch_row();
-        echo "<h1>" . $article[2] ."</h1>";
-        echo "<h4>This article is all about " . $article[2] ."!</h4>";
         $title = $article[1];
         $info = $article[2];
-        echo $article[3];
-        //add link to addArticle here
-      }
-      else{
-        echo "<h1>Home</h1>";
-        echo "<h4>Use the navigation bar to view wikis, or add you own below!</h4>";
-        echo "<form action='addArticle.php' method='post'>
-          <input type='submit' value='Upload Article' name='articleSubmit'>
-        </form>";
-      }
-    ?>
+        ?>
+        <div class="articletitle">
+            <!-- print article title -->
+            <?php echo "<h1>" . $article[2] . "</h1>"; ?>
+            <div class="addCreateLinks">
+              <a href="editArticle.php">Edit Article</a>
+              <a href="addArticle.php">Create New Article</a>
+            </div>
+          </div>
+          <div class="square">
+            <div><img src="./noise.png"></div>
+            <!-- print article body -->
+            <?php echo "<p>" . $article[3] . "</p>"; ?>
+          </div>
+        </div>
+    </div>
   </div>
   <?php include "footer.php";?>
 </body>
