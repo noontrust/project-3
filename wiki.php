ee05a6372447e52d68e036ba655d4019480d989b
LColeman137 <116465104+LColeman137@users.noreply.github.com>
2022-11-01 19:45:53 -0500

    Add files via upload

--- wiki.php
+++ wiki.php
@@ -1,69 +1,51 @@
   <?php
-  //include header and footer
-  /*
-  if session not created redirect to createUser
+  include "header.php";
+  include "database.php";
+  //if session not created redirect to login
   session_start();
-  if(!isset($_SESSION["sessionId"])){header("Location: createUser.php";
-  include "database.php";}
-*/
-    include "header.php";
-    session_start();
-    if(!$_SESSION["isLoggedIn"]){
-      header("Location: login.php");
-    }
-    //query the articles
-    $HOST = "localhost";
-    $USERNAME = "root";
-    $PASSWORD = "";
-    $DB_NAME = "wiki";
-    $conn = new mysqli($HOST, $USERNAME, $PASSWORD, $DB_NAME);
-    $stmt = $conn->query("SELECT * FROM article");
-    $wikiResult = $stmt->fetch_all();
+  if(!isset($_SESSION["isLoggedIn"])){
+    header("Location: login.php");
+  }
   ?>
 <html lang="en">
-    <head>
-      <title>Wiki of Pets</title>
-      <meta charset="utf-8">
-      <link href="style.css" type="text/css" rel="stylesheet">
-    </head>
-    <body>
-      <div class="bigcontainer">
-      <div class="container">
-      <div class="sideBar">
-        <?php
-        //possible search bar
-        //place links to each wiki page
-        foreach ($wikiResult as $row){
-          echo "<a href=wiki.php?shortTitle=$row[1] title='$row[2]'>$row[2]</a>";
-        }
-        ?>
-      </div>
-      <div class="articleDivs">
-        <?php
-            $shortTitle = $_GET["shortTitle"];
-            unset($_GET["shortTitle"]);
-            $articleQuery = "SELECT * FROM article WHERE article_short_title = '$shortTitle';";
-            $stmt = $conn->query($articleQuery);
-            $article = $stmt->fetch_row();
-            $title = $article[1];
-            $info = $article[2];
-            ?>
-            <div class="articletitle">
-              <!-- print article title -->
-              <?php echo "<h1>" . $article[2] . "</h1>"; ?>
-              <div class="addCreateLinks">
-                <a href="editArticle.php">Edit Article</a>
-                <a href="add.php">Create New Article</a>
-              </div>
-            </div>
-            <div class="square">
-              <div><img src="./noise.png"></div>
-              <!-- print article body -->
-              <?php echo "<p>" . $article[3] . "</p>"; ?>
-            </div>
-      </div>
-    </div>
+<head>
+  <title>Cool Facts Wiki</title>
+  <meta charset="utf-8">
+  <link href="style.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+  <div class="sideBar">
+    <?php
+    //place links to newest 5 wiki page
+     foreach ($topArticle as $row){
+      echo "<a href=wiki.php?shortTitle=$row[1] title='$row[2]'>$row[2]</a>";
+    }
+    ?>
+  </div>
+  <div class="main">
+    <?php
+      if(isset($_GET["shortTitle"])){
+        $shortTitle = $_GET["shortTitle"];
+        unset($_GET["shortTitle"]);
+        $articleQuery = "SELECT * FROM article WHERE article_short_title = '$shortTitle';";
+        $stmt = $conn->query($articleQuery);
+        $article = $stmt->fetch_row();
+        echo "<h1>" . $article[2] ."</h1>";
+        echo "<h4>This article is all about " . $article[2] ."!</h4>";
+        $title = $article[1];
+        $info = $article[2];
+        echo $article[3];
+        //add link to addArticle here
+      }
+      else{
+        echo "<h1>Home</h1>";
+        echo "<h4>Use the navigation bar to view wikis, or add you own below!</h4>";
+        echo "<form action='addArticle.php' method='post'>
+          <input type='submit' value='Upload Article' name='articleSubmit'>
+        </form>";
+      }
+    ?>
   </div>
   <?php include "footer.php";?>
-    </body>
+</body>
 </html>
